65e5c6b11553f2e0f6b96160078f062aa501f68c
Klaudia Bogacka <klaudiaciszek9@gmail.com>
2023-02-05 21:23:38 +0100

    Revert "Zadanie domowe dzień 4"
    This reverts commit 122ad4bfd485da6eb175a44228609e748986c72f.

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,21 +1,2 @@
-﻿string name = "Ewa";
-string gender = "Kobieta";
-string gender2 = "Mezczyzna";
-int age = 17;
-
-if(age < 30 && gender == "Kobieta")
-    {
-        Console.WriteLine("Kobieta poniżej 30 lat");
-    }
-else if(gender == "Kobieta" && age == 33 && name == "Ewa")
-    {
-        Console.WriteLine("Ewa, lat 30");
-    }
-else if(name == "Karol" && gender2 == "mężczyzna" && age <= 17)
-    {
-        Console.WriteLine("Niepełnoletni mężczyzna");
-    }
-else
-{
-    Console.WriteLine("To jest Ania i ma 40 lat");
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
