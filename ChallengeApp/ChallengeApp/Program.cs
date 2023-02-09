75b3f0257a84964bf04ccfd8c1498d2a337c5aa0
Klaudia Bogacka <klaudiaciszek9@gmail.com>
2023-02-09 10:32:24 +0100

    Zadanie domowe dzień 4 - poprawione

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string name = "Ewa";
+bool women = true;
+int age = 17;
+
+if (name == "Ewa" && age < 30 )
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (women == true && age != 33 && name == "Ewa")
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (name == "Karol" && women != true && age <= 17)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("To jest Ewa i ma 30 lat");
+}
