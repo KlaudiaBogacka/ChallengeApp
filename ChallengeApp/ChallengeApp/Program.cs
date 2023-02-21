b04f0bd16fbf92a82685e2271975ef58868bbb9f
Klaudia Bogacka <klaudiaciszek9@gmail.com>
2023-02-21 17:07:35 +0100

    zadanie dodatkowe

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,74 +1,40 @@
-﻿using System.Diagnostics.Metrics;
+﻿int[] grades = new int[32];
 
-int number = 4594;
+List<string> letterOfAlpabet = new List<string>();
+letterOfAlpabet.Add("A");
+letterOfAlpabet.Add("Ą");
+letterOfAlpabet.Add("B");
+letterOfAlpabet.Add("C");
+letterOfAlpabet.Add("Ć");
+letterOfAlpabet.Add("D");
+letterOfAlpabet.Add("E);
+letterOfAlpabet.Add("Ę");
+letterOfAlpabet.Add("F");
+letterOfAlpabet.Add("G");
+letterOfAlpabet.Add("H");
+letterOfAlpabet.Add("I");
+letterOfAlpabet.Add("J");
+letterOfAlpabet.Add("K");
+letterOfAlpabet.Add("L");
+letterOfAlpabet.Add("Ł");
+letterOfAlpabet.Add("M");
+letterOfAlpabet.Add("N");
+letterOfAlpabet.Add("Ń");
+letterOfAlpabet.Add("O");
+letterOfAlpabet.Add("Ó");
+letterOfAlpabet.Add("P");
+letterOfAlpabet.Add("R");
+letterOfAlpabet.Add("S");
+letterOfAlpabet.Add("Ś");
+letterOfAlpabet.Add("T");
+letterOfAlpabet.Add("U");
+letterOfAlpabet.Add("W");
+letterOfAlpabet.Add("Y");
+letterOfAlpabet.Add("Z");
+letterOfAlpabet.Add("Ź");
+letterOfAlpabet.Add("Ż");
 
-List<string> numbers = new List<string>();
-string numberAsString = number.ToString();
-char[] letters = numberAsString.ToArray();
-
-int counter0 = 0;
-int counter1 = 0;
-int counter2 = 0;
-int counter3 = 0;
-int counter4 = 0;
-int counter5 = 0;
-int counter6 = 0;
-int counter7 = 0;
-int counter8 = 0;
-int counter9 = 0;
-
-foreach (char letter in letters)
+foreach(var letter in letterOfAlphabet)
 {
-    if (letter == '0')
-    {
-        counter0++;
-    }
-    else if (letter == '1')
-    {
-        counter1++;
-    }
-    else if (letter == '2')
-    {
-        counter2++;
-    }
-    else if (letter == '3')
-    {
-        counter3++;
-    }
-    else if (letter == '4')
-    {
-        counter4++;
-    }
-    else if (letter == '5')
-    {
-        counter5++;
-    }
-    else if (letter == '6')
-    {
-        counter6++;
-    }
-    else if (letter == '7')
-    {
-        counter7++;
-    }
-    else if (letter == '8')
-    {
-        counter8++;
-    }
-    else if (letter == '9')
-    {
-        counter9++;
-    }
+    Console.WriteLine(letter);
 }
-
-Console.WriteLine(" w liczbie 4594 ");
-Console.WriteLine(" liczba 0 " + " występje " + counter0 + " razy ");
-Console.WriteLine(" liczba 1 " + " występje " + counter1 + " razy ");
-Console.WriteLine(" liczba 2 " + " występje " + counter2 + " razy ");
-Console.WriteLine(" liczba 3 " + " występje " + counter3 + " razy ");
-Console.WriteLine(" liczba 4 " + " występje " + counter4 + " razy ");
-Console.WriteLine(" liczba 5 " + " występje " + counter5 + " raz ");
-Console.WriteLine(" liczba 6 " + " występje " + counter6 + " razy ");
-Console.WriteLine(" liczba 7 " + " występje " + counter7 + " razy ");
-Console.WriteLine(" liczba 8 " + " występje " + counter8 + " razy ");
-Console.WriteLine(" liczba 9 " + " występje " + counter9 + " razy ");
