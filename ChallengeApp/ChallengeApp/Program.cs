686ddee03a3ed832cc1abe91026124cfc5039cb9
Klaudia Bogacka <klaudiaciszek9@gmail.com>
2023-03-05 22:54:24 +0100

    Zadanie domowe z dnia 6.

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,40 +1,57 @@
-﻿int[] grades = new int[32];
+﻿using ChallengeApp;
 
-List<string> letterOfAlpabet = new List<string>();
-letterOfAlpabet.Add("A");
-letterOfAlpabet.Add("Ą");
-letterOfAlpabet.Add("B");
-letterOfAlpabet.Add("C");
-letterOfAlpabet.Add("Ć");
-letterOfAlpabet.Add("D");
-letterOfAlpabet.Add("E);
-letterOfAlpabet.Add("Ę");
-letterOfAlpabet.Add("F");
-letterOfAlpabet.Add("G");
-letterOfAlpabet.Add("H");
-letterOfAlpabet.Add("I");
-letterOfAlpabet.Add("J");
-letterOfAlpabet.Add("K");
-letterOfAlpabet.Add("L");
-letterOfAlpabet.Add("Ł");
-letterOfAlpabet.Add("M");
-letterOfAlpabet.Add("N");
-letterOfAlpabet.Add("Ń");
-letterOfAlpabet.Add("O");
-letterOfAlpabet.Add("Ó");
-letterOfAlpabet.Add("P");
-letterOfAlpabet.Add("R");
-letterOfAlpabet.Add("S");
-letterOfAlpabet.Add("Ś");
-letterOfAlpabet.Add("T");
-letterOfAlpabet.Add("U");
-letterOfAlpabet.Add("W");
-letterOfAlpabet.Add("Y");
-letterOfAlpabet.Add("Z");
-letterOfAlpabet.Add("Ź");
-letterOfAlpabet.Add("Ż");
+Employee employee1 = new Employee(" Karol ", " 28", " Krawczyk ");
+Employee employee2 = new Employee(" Matrta ", "40", " Kowalska ");
+Employee employee3 = new Employee(" Irena " , "35", " Nowak ");
 
-foreach(var letter in letterOfAlphabet)
+employee1.AddScore(5);
+employee1.AddScore(2);
+employee1.AddScore(9);
+employee1.AddScore(4);
+employee1.AddScore(2);
+
+employee2.AddScore(3);
+employee2.AddScore(5);
+employee2.AddScore(9);
+employee2.AddScore(4);
+employee2.AddScore(8);
+
+employee3.AddScore(9);
+employee3.AddScore(5);
+employee3.AddScore(7);
+employee3.AddScore(4);
+employee3.AddScore(1);
+
+List<Employee> employees= new List<Employee>()
 {
-    Console.WriteLine(letter);
+    employee1, employee2, employee3
+};
+
+int maxResult = -1;
+Employee employeeWithMaxResult = null;
+
+foreach(var Employee in employees)
+{
+    if (Employee.Result > maxResult)
+    {
+        maxResult = Employee.Result;
+        employeeWithMaxResult = Employee;
+    }
+}
+
+if(employee1.Result >= employee2.Result && employee1.Result >= employee3.Result)
+{
+    Console.WriteLine(" Najlepszy wynik uzyskał/a " + employee1.Name + employee1.Surname + employee1.Age + " lat " + " z wynikiem " + employee1.Result);
+}
+else
+{
+    if (employee2.Result >= employee1.Result && employee2.Result >= employee3.Result)
+    {
+        Console.WriteLine(" Najlepszy wynik uzyskał/a " + employee2.Name + employee2.Surname + employee2.Age + " lat " + " z wynikiem " + employee2.Result);
+    }
+
+    else
+    {
+        Console.WriteLine(" Najlepszy wynik uzyskał/a " + employee3.Name + employee3.Surname + employee3.Age + " lat " + " z wynikiem " + employee3.Result);
+    }
 }
