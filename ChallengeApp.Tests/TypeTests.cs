03ad48ee4e91d69abbeb99ee23cf1e214befc494
klaudiaciszek9@gmail.com <klaudiaciszek9@gmail.com>
2023-07-26 16:28:06 +0200

    Zadanie domowe z dnia 8

---
+++ ChallengeApp.Tests/TypeTests.cs
@@ -0,0 +1,37 @@
+﻿namespace ChallengeApp.Tests
+{
+    public class TypeTests
+    {
+        [Test]
+        public void TEST()
+        {
+            // arrange
+            int number1 = 1;
+            int number2 = 2;
+            // act
+
+
+            //assert 
+            Assert.AreNotEqual(number1, number2);
+        }
+
+        [Test]
+        public void GetEmployeeShouldReturnDifferentObjects()
+        {
+            // arrange
+            var employee1 = GetEmployee("Klaudia", "Nowak", 31);
+            var employee2 = GetEmployee("Damian", "Kowalski", 33);
+            // act
+
+
+            //assert 
+            Assert.AreNotEqual(employee1, employee2);
+        }
+
+        private Employee GetEmployee(string name, string surname, int age)
+        {
+            return new Employee(name, surname, age);
+        }
+    }
+    
+}
