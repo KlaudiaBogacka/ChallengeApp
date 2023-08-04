cb834bb567e2e7b34dfcb42646bf8072c91a59c3
klaudiaciszek9@gmail.com <klaudiaciszek9@gmail.com>
2023-08-04 15:38:12 +0200

    Zadanie domowe z dnia 14

--- ChallengeApp/Employee.cs
+++ ChallengeApp/Employee.cs
@@ -2,18 +2,16 @@
 
 namespace ChallengeApp
 {
-    public class Employee
+    public class Employee : Person
     {
-        private const char sex = 'M';
-
         private List<float> grades = new List<float>();
 
-        public Employee(string name, string surname)
+
+        public Employee(string name, string surname, char sex, int age)
+            : base(name, surname, sex, age)
         {
-            this.Name = name;
-            this.Surname = surname;
+
         }
-        public string Name { get; private set; }
 
         public string Surname { get; private set; }
 
---
+++ ChallengeApp/Person.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ChallengeApp
+{
+    public abstract class Person : System.Object
+    {
+        public Person(string name, string surname, char sex, int age)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.sex = sex;
+            this.age = age;
+
+        }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public char sex { get; private set; }
+        public int age { get; private set; }
+    }
+}
